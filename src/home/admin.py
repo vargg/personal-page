591c3139643afae471794098f583ad7d9a4a05ff
vargg <smrglwp@outlook.com>
2024-11-15 14:07:53 +0300

    do not use raw literals

--- src/home/admin.py
+++ src/home/admin.py
@@ -3,75 +3,87 @@ from django.contrib import admin
 from . import models
 
 
+class DK:
+    created_at = "created_at"
+    details_list = "details_list"
+    id_ = "id"
+    is_active = "is_active"
+    items = "items"
+    link_list = "link_list"
+    name = "name"
+    tag = "tag"
+    updated_at = "updated_at"
+
+
 class BaseAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "created_at", "updated_at")
-    list_display_links = ("id", "name")
-    search_fields = ("name",)
+    list_display = (DK.id_, DK.name, DK.created_at, DK.updated_at)
+    list_display_links = (DK.id_, DK.name)
+    search_fields = (DK.name,)
 
 
+@admin.register(models.Asset)
 class AssetAdmin(BaseAdmin):
-    list_display = ("id", "name", "tag", "created_at","updated_at")
-    list_filter = ("tag",)
+    list_display = (DK.id_, DK.name, DK.tag, DK.created_at, DK.updated_at)
+    list_filter = (DK.tag,)
 
 
+@admin.register(models.StartScreen)
 class StartScreenAdmin(BaseAdmin):
     pass
 
 
+@admin.register(models.Promo)
 class PromoAdmin(BaseAdmin):
     pass
 
 
+@admin.register(models.ServiceItem)
 class ServiceItemAdmin(BaseAdmin):
     pass
 
 
+@admin.register(models.ServiceBlock)
 class ServiceBlockAdmin(BaseAdmin):
-    filter_horizontal = ("items",)
+    filter_horizontal = (DK.items,)
 
 
+@admin.register(models.Services)
 class ServicesAdmin(BaseAdmin):
-    filter_horizontal = ("items",)
+    filter_horizontal = (DK.items,)
 
 
+@admin.register(models.Portfolio)
 class PortfolioAdmin(BaseAdmin):
-    filter_horizontal = ("items",)
+    filter_horizontal = (DK.items,)
 
 
+@admin.register(models.AboutMe)
 class AboutMeAdmin(BaseAdmin):
     pass
 
 
+@admin.register(models.ContactLink)
 class ContactLinkAdmin(BaseAdmin):
     pass
 
 
+@admin.register(models.ContactDetail)
 class ContactDetailAdmin(BaseAdmin):
     pass
 
 
+@admin.register(models.Contacts)
 class ContactsAdmin(BaseAdmin):
-    filter_horizontal = ("details_list", "link_list")
+    filter_horizontal = (DK.details_list, DK.link_list)
 
 
+@admin.register(models.Footer)
 class FooterAdmin(BaseAdmin):
     pass
 
 
+@admin.register(models.Page)
 class PageAdmin(BaseAdmin):
-    list_display = ("id", "name", "is_active", "created_at","updated_at")
-
-
-admin.site.register(models.Asset, AssetAdmin)
-admin.site.register(models.StartScreen, StartScreenAdmin)
-admin.site.register(models.Promo, PromoAdmin)
-admin.site.register(models.ServiceItem, ServiceItemAdmin)
-admin.site.register(models.ServiceBlock, ServiceBlockAdmin)
-admin.site.register(models.Services, ServicesAdmin)
-admin.site.register(models.Portfolio, PortfolioAdmin)
-admin.site.register(models.AboutMe, AboutMeAdmin)
-admin.site.register(models.ContactDetail, ContactDetailAdmin)
-admin.site.register(models.ContactLink, ContactLinkAdmin)
-admin.site.register(models.Contacts, ContactsAdmin)
-admin.site.register(models.Footer, FooterAdmin)
-admin.site.register(models.Page, PageAdmin)
+    list_display = (
+        DK.id_, DK.name, DK.is_active, DK.created_at, DK.updated_at
+    )
