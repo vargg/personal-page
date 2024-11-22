e8b75bd4f371e00ed550da8810ca5658053b2d34
vargg <smrglwp@outlook.com>
2024-11-22 21:36:02 +0300

    try to get the page from the db using a more efficient query

--- src/home/views.py
+++ src/home/views.py
@@ -28,8 +28,47 @@ def _make_context_for_page(page: Page) -> dict[str, t.Any]:
     }
 
 
+def _get_first_page(filter_kwargs: dict) -> Page | None:
+    selected = (
+        "icon",
+        "shortcut_icon",
+        "logo",
+        "start_screen",
+        "start_screen__logo",
+        "start_screen__bg_pattern",
+        "promo",
+        "services",
+        "portfolio",
+        "about_me",
+        "about_me__avatar",
+        "about_me__bg_image",
+        "contacts",
+        "footer",
+        "footer__logo",
+        "footer__bg_pattern",
+    )
+    excluded_fields = ("id", "note", "created_at", "updated_at")
+    defered = []
+    for head in selected:
+        defered.extend(f"{head}__{tail}" for tail in excluded_fields)
+
+    return Page.objects.filter(**filter_kwargs).select_related(
+        *selected
+    ).prefetch_related(
+        "services__items__image",
+        "services__items__items",
+        "portfolio__items",
+        "contacts__details_list",
+        "contacts__link_list",
+    ).defer(
+        *excluded_fields,
+        *defered,
+        "is_active",
+    ).first()
+
+
 def index(request: HttpRequest) -> HttpResponse:
-    page = Page.objects.filter(is_active=True).first()
+    page = _get_first_page({"is_active": True})
     if not page:
         return HttpResponseNotFound()
 
@@ -38,7 +77,7 @@ def index(request: HttpRequest) -> HttpResponse:
 
 @user_passes_test(lambda user: user.is_superuser)
 def preview(request: HttpRequest, pk: int) -> HttpResponse:
-    page = Page.objects.filter(pk=pk).first()
+    page = _get_first_page({"pk": pk})
     if not page:
         return HttpResponseNotFound()
 
